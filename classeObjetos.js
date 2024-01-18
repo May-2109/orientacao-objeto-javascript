94efc8e89839f46a54dd3f755d92d683e6d68513
May-2109 <sempremy@gmail.com>
2024-01-18 15:03:03 -0300

    atualizado

---
+++ classeObjetos.js
@@ -0,0 +1,26 @@
+// classe => padroniza o formato de uma estrutura de dados
+// objeto => mantem a padronização da classe e implementa seus valores das propriedades, 
+// além de termos métodos inteligentes e funções próprias 
+
+class formaDeBolo{
+    constructor(saborDaMassa, saborRecheio){
+        this.saborDaMassa = saborDaMassa
+        this.saborRecheio = saborRecheio
+    }
+
+    escrever(){
+        console.log(`Um delicioso bolo de ${this.saborDaMassa} com recheio de ${this.saborRecheio}`)
+    }
+
+    assar(){
+        console.log("Bolo assando de " + this.saborDaMassa)
+    }
+}
+
+let boloFesta = new formaDeBolo("chocolate", "nutella")
+let boloPremiun = new formaDeBolo("baunilha", "coco")
+
+boloFesta.escrever()
+
+boloPremiun.escrever()
+boloPremiun.assar()

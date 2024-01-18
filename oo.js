6bd09ee2f478d737437236f8613012f8c13d691d
May-2109 <sempremy@gmail.com>
2024-01-18 12:48:00 -0300

    terminado aula

--- oo.js
+++ oo.js
@@ -1,1 +1,29 @@
-console.log("may")
+// json => JavaScript Object Notation
+//chave e valor com o objetivo de transferir dados
+
+let invoice = {
+    name: "Mayara", 
+    age: 31, 
+    products: {
+        0: ["mouse",  29.90],
+        1: ["teclado mecanico", 129.99],
+        2: ["monitor", 899,99],
+        3: ["TV de 100''",  1000.00]
+    } ,
+    taxes: "98.90"   
+}
+
+generateInvoice(invoice)
+
+function generateInvoice(invoice){
+    console.log(`O comprador é ${invoice.name}` )
+    console.log(`A idade é ${invoice.age} ` )
+    console.log("--------------------")
+
+    for (let index in invoice.products){
+        let [productName, productPrice] = invoice.products[index]
+        console.log(`- ${index} - ${productName}: R$ ${productPrice}`)
+    }
+    
+    
+}
